15e508bd0e72b640d71bc383236bad1086f95597
Pedro Vinícius Angel Edreira Edreira <112908326+pedroedreira@users.noreply.github.com>
2022-10-20 00:53:54 +0000

    Aprendi a criar variáveis e exibir na tela

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # print() = comando de saída
 
 print("Alo Mundo!")
+
+# Quando quiser guardar uma string (frase)
+nome = "Pedro Edreira"
+# Quando quiser guardar um número inteio
+idade = 22
+
+# Exibir o nome da variável "nome"
+
+print(nome)
