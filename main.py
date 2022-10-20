446705334e3da7f5f3b30b7ca5fdaab78bb876c1
Pedro Vinícius Angel Edreira Edreira <112908326+pedroedreira@users.noreply.github.com>
2022-10-20 01:16:03 +0000

    Aprendi a criar variáveis e exibir na tela

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # Meu primeiro projeto Python
-# print() = comando de saída
 
+# print() = comando de saída
 print("Alo Mundo!")
 
 # Quando quiser guardar uma string (frase)
 nome = "Pedro Edreira"
+
 # Quando quiser guardar um número inteio
 idade = 22
 
 # Exibir o nome da variável "nome"
-
 print(nome)
+#Ou
+print("Meu nome é "+nome)
+
+# Quando quiser exibir a frase "Minha idade é" complementando com o conteúdo da variável "idade"
+print("Eu tenho "+str(idade)+" anos.")
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+# Quando quiser exibir "meu nome é xxx e tenho xxx anos" usando as variáveis
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
